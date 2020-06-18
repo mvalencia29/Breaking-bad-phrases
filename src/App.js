65defba420b8b49dee8fcadd4335df2a758ba951
Miguel <miguevalencia29@hotmail.com>
2020-06-18 01:23:49 -0500

    Completando el diseño

--- src/App.js
+++ src/App.js
@@ -1,8 +1,55 @@
-import React from 'react';
+import React, { Fragment } from "react";
+import styled from "styled-components";
+import { Button } from "react-bootstrap";
+
+const ContainerFrase = styled.div`
+  margin-top: 10%;
+  margin-left: 60px;
+  width: 40%;
+  word-break: break-all;
+  padding: 15px;
+  -webkit-appearance: none;
+`;
+
+const Frase = styled.p`
+  text-align: center;
+  -webkit-appearance: none;
+`;
+
+const EscritoPor = styled.p`
+  text-align: end;
+`;
+
+const ContainerButton = styled.div`
+  margin-top: 5%;
+  margin-left: 75px;
+  width: 40%;
+  padding: 15px;
+  text-align: center;
+`;
+
+const consultarApi = () => {
+  
+};
 
 function App() {
   return (
-    <p>Hola</p>
+    <Fragment>
+      <ContainerFrase>
+        <Frase>
+          Hobsyuddddddddddddddddddddddddddddddddddddddddddddddddd
+          ddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+          ddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+          dddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+        </Frase>
+        <EscritoPor>FYTGUJHJL</EscritoPor>
+      </ContainerFrase>
+      <ContainerButton>
+        <Button variant="outline-dark" onClick={consultarApi}>
+          Obtener Frase
+        </Button>
+      </ContainerButton>
+    </Fragment>
   );
 }
 
